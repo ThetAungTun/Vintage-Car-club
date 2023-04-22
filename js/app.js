b8f7b14ce1b451a275a1f0cdb617209b9c6187f1
Thet Aung Tun <thetaung7826@gmail.com>
2023-04-22 15:26:42 +0630

    Update

--- js/app.js
+++ js/app.js
@@ -4,3 +4,32 @@ menuIcon.addEventListener("click", function () {
   menuList.classList.toggle("hidden");
   console.log(menuList);
 });
+
+const activePage = window.location.pathname;
+//console.log(activePage);
+
+const links = document.querySelectorAll("nav a");
+//console.log(links);
+
+links.forEach((link) => {
+  if (link.href.includes(activePage)) {
+    link.classList.add("active");
+  }
+});
+
+const imageSection = document.querySelectorAll(".new");
+console.log(imageSection);
+
+const imageReval = function (enteries, observer) {
+  enteries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove("lazy");
+  });
+};
+
+const imageObserver = new IntersectionObserver(imageReval, {
+  threshold: 0.3,
+});
+
+imageSection.forEach((image) => imageObserver.observe(image));
