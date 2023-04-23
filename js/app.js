6f175d02585b01c40479232d783d9b6b9e392932
Thet Aung Tun <thetaung7826@gmail.com>
2023-04-23 10:45:30 +0630

    final home page

--- js/app.js
+++ js/app.js
@@ -34,3 +34,36 @@ const imageObserver = new IntersectionObserver(imageReval, {
 });
 
 imageSection.forEach((image) => imageObserver.observe(image));
+
+const slides = document.querySelectorAll(".slide");
+
+const btnRight = document.querySelector(".slider__btn--right");
+const btnLeft = document.querySelector(".slider__btn--left");
+
+slides.forEach((s, i) => {
+  s.style.transform = `translateX( ${100 * i}%)`;
+});
+
+let curSlide = 0;
+let maxSlide = slides.length;
+btnRight.addEventListener("click", function () {
+  if (curSlide == maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX( ${100 * (i - curSlide)}%)`;
+  });
+});
+
+btnLeft.addEventListener("click", function () {
+  if (curSlide == 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX( ${100 * (i - curSlide)}%)`;
+  });
+});
